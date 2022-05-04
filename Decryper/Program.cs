764ec3052056439bd0fdad25e1da4c29c61cfe6a
JoelGrullon12 <jjj007507388@gmail.com>
2022-05-04 00:00:39 -0400

    Agregar archivos de proyecto.

---
+++ Decryper/Program.cs
@@ -0,0 +1,112 @@
+﻿using Data;
+
+namespace Decryper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary p = new Dictionary();
+
+            Console.WriteLine("Desencriptar Textos");
+
+            string t, o, k;
+
+            t = o = k = "";
+            string text = "";
+
+
+            //tar[t.Length - 1] = 1;
+
+            /*
+            if(t.Lenght!=o.Lenght){
+            	Console.WriteLine("La longitud del texto no coincide con el orden binario, ");
+            }
+            */
+
+            Console.WriteLine();
+            Console.WriteLine("Escriba el texto encriptado: ");
+            t = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Escriba el orden binario: ");
+            o = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Escriba la clave numerica: ");
+            k = Console.ReadLine();
+
+            //Console.WriteLine(o);
+
+
+            int[] tar = new int[t.Length];
+            int[] oar = new int[o.Length];
+            int[] kar = new int[k.Length];
+
+            int u = 0;
+            foreach (char c in t)
+            {
+                tar[u] = p.L2N(c.ToString());
+                u++;
+            }
+
+
+            //for (int i = 0; i < tar.Length; i++)
+            //{
+            //	Console.WriteLine(tar[i]);
+            //}
+
+            u = 0;
+            foreach (char c in o)
+            {
+                oar[u] = Convert.ToInt32(c.ToString());
+                u++;
+            }
+
+            u = 0;
+            foreach (char c in k)
+            {
+                kar[u] = Convert.ToInt32(c.ToString());
+                u++;
+            }
+
+
+            //for (int i = 0; i < oar.Length; i++)
+            //{
+            //    Console.WriteLine(oar[i]);
+            //}//
+
+
+            for (int i = 0; i < tar.Length; i++)
+            {
+                if (oar[i] == 1)
+                {
+                    tar[i] += kar[i];
+                }
+                else if (oar[i] == 0)
+                {
+                    tar[i] -= kar[i];
+                }
+                else
+                {
+                    Console.WriteLine("Se ha encontrado un error en el orden binario, reviselo e intentelo de nuevo");
+                }
+
+                if (tar[i] > 28)
+                    tar[i] -= 28;
+
+                if (tar[i] < 0)
+                    tar[i] += 28;
+
+                text += p.N2L(tar[i]);
+
+                //Console.WriteLine(tar[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(text);
+
+            //*/
+        }
+    }
+}
---
+++ Encryper/Program.cs
@@ -0,0 +1,69 @@
+﻿using Data;
+
+namespace Encryper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary p=new Dictionary();
+
+            string text;
+            string[] letters;
+
+            Console.Write("Escriba el texto que quiera encriptar: ");
+            text = Console.ReadLine();
+
+            text = text.ToUpper();
+
+            letters=new string[text.Length];
+
+            int o = 0;
+
+            foreach(char c in text)
+            {
+                letters[o] = c.ToString();
+                o++;
+            }
+
+            //int[] l2n=new Int32[letters.Length];
+
+            string t = "", n = "", k = "";
+
+            Random r = new Random();
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                int l2n = p.L2N(letters[i]);
+
+                int UoD = r.Next(2);
+
+                int num = r.Next(10);
+
+                int n2n = UoD == 1 ? l2n - num : l2n + num;
+
+                if (n2n < 0)
+                    n2n += 28;
+
+                if (n2n > 28)
+                    n2n -= 28;
+
+                t += p.N2L(n2n);
+                n += num.ToString();
+                k += UoD;
+
+                //Console.WriteLine(letters[i]);
+            }
+
+            Console.WriteLine(t);
+            Console.WriteLine(n);
+            Console.WriteLine(k);
+
+            //for (int i = 0; i < 30; i++)
+            //{
+            //    Console.WriteLine(r.Next(2).ToString());
+            //}
+
+        }
+    }
+}
